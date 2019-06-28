35646b4566811182197342ac23642fa5cb0437de
bcorbett0312 <benjamin.t.corbett@gmail.com>
2019-06-28 09:23:00 -0400

    Completed

--- src/main/java/HamletParser.java
+++ src/main/java/HamletParser.java
@@ -1,6 +1,8 @@
 import java.io.File;
 import java.io.IOException;
 import java.util.Scanner;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * Created by thook on 10/7/15.
@@ -8,6 +10,8 @@ import java.util.Scanner;
 public class HamletParser {
 
     private String hamletData;
+    private String horatioData;
+    private String hamletReplace;
 
     public HamletParser(){
         this.hamletData = loadFile();
@@ -32,8 +36,48 @@ public class HamletParser {
         return result.toString();
     }
 
+
+
+
     public String getHamletData(){
         return hamletData;
+    }
+
+    public String changeHamlet(String changeTo){
+        String result = hamletData.replaceAll("(?i)Hamlet", changeTo);
+        return result;
+
+    }
+
+    public String changeHoratio(String changeTo){
+        String result = hamletData.replaceAll("(?i)Horatio", changeTo);
+        return result;
+
+    }
+
+    public Integer findHamlet(String toCheck){
+        Pattern patternToMatch = Pattern.compile("(?i)Hamlet");
+        Matcher matcher = patternToMatch.matcher(toCheck);
+        Integer counter = 0;
+        while(matcher.find()){
+            counter++;
+
+
+        }
+        return counter;
+
+    }
+
+    public Integer findHoratio(String toCheck){
+        Pattern horatioMatcher = Pattern.compile("(?i)Horatio");
+        Matcher matcher = horatioMatcher.matcher(toCheck);
+        Integer counter = 0;
+        while(matcher.find()){
+            counter++;
+
+        }
+        return counter++;
+
     }
 
 }
--- src/test/java/HamletParserTest.java
+++ src/test/java/HamletParserTest.java
@@ -3,6 +3,8 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
+
+
 public class HamletParserTest {
     private String hamletText;
     private HamletParser hamletParser;
@@ -15,17 +17,43 @@ public class HamletParserTest {
 
     @Test
     public void testChangeHamletToLeon() {
+        String toEval = hamletParser.changeHamlet("Leon");
+
+        Integer expected = 0;
+
+        assertEquals(expected, hamletParser.findHamlet(hamletParser.changeHamlet("Leon")));
+
+        assertTrue(toEval.contains("Leon"));
+
     }
 
     @Test
     public void testChangeHoratioToTariq() {
+        Integer expected = 0;
+        String toEval = hamletParser.changeHoratio("Tariq");
+        assertEquals(expected, hamletParser.findHoratio(toEval));
+
+        assertTrue(toEval.contains("Tariq"));
+
     }
 
     @Test
     public void testFindHoratio() {
+        Integer expected = 158;
+
+
+
+        assertEquals(expected, hamletParser.findHoratio(hamletText));
+
+
     }
 
     @Test
     public void testFindHamlet() {
+        Integer expected = 472;
+
+
+        assertEquals(expected, hamletParser.findHamlet(hamletText));
+
     }
 }
